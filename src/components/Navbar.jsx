371abded5516295b23c78ae9c444a20178ff0677
Dev-Farhan <mohdfarhan0678@.com>
2025-05-09 07:18:48 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,6 @@ const Navbar = () => {
       <div className="flex flex-shrink-0 items-center">
         <img className="mx-2 w-14" src={Logo} alt="Logo" />
       </div>
-      <div className="bg-white text-black dark:bg-black dark:text-white p-4">
-        This adapts to dark mode!
-      </div>
-
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <a
           href="https://www.linkedin.com/in/mohd-farhan0678"
